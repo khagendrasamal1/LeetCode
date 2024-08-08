492d94fe6ff64918f0f6f0dc9e1a6552586dd918
Khagendra Samal <113529958+ImAlokdeCoder@users.noreply.github.com>
2024-08-08 13:44:33 +0530

    Time: 2 ms (72.51%), Space: 42.5 MB (36.92%) - LeetHub

---
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -0,0 +1,59 @@
+class Solution {
+    static class Pair {
+        int row, col, tm;
+        Pair(int r, int c, int t) {
+            this.row = r;
+            this.col = c;
+            this.tm = t;
+        }
+    }
+    
+    public int orangesRotting(int[][] grid) {
+        int n = grid.length;
+        int m = grid[0].length;
+
+        Queue<Pair> q = new LinkedList<>();
+        int vis[][] = new int[n][m];
+        int fresh = 0;
+
+        // Initialize the queue with all rotten oranges and count fresh oranges
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] == 2) {
+                    q.add(new Pair(i, j, 0));
+                    vis[i][j] = 2;
+                } else {
+                    vis[i][j] = 0;
+                }
+                if (grid[i][j] == 1) fresh++;
+            }
+        }
+
+        int tm = 0;
+        int drow[] = {-1, 0, 1, 0};
+        int dcol[] = {0, 1, 0, -1};
+
+        while (!q.isEmpty()) {
+            int r = q.peek().row;
+            int c = q.peek().col;
+            int t = q.peek().tm;
+            tm = Math.max(tm, t);
+            q.remove();
+
+            for (int i = 0; i < 4; i++) {
+                int nrow = r + drow[i];
+                int ncol = c + dcol[i];
+
+                if (nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && vis[nrow][ncol] == 0 && grid[nrow][ncol] == 1) {
+                    q.add(new Pair(nrow, ncol, t + 1));
+                    vis[nrow][ncol] = 2;
+                    fresh--;
+                }
+            }
+        }
+
+        // If there are still fresh oranges, return -1
+        if (fresh > 0) return -1;
+        return tm;
+    }
+}
