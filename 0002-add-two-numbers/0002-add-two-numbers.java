9c953e4121c4979a3186fa2237ce6637f3791466
Khagendra Samal <113529958+khagendrasamal1@users.noreply.github.com>
2024-09-08 13:53:54 +0530

    Time: 1 ms (100%), Space: 44.5 MB (40.9%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.java
+++ 0002-add-two-numbers/0002-add-two-numbers.java
@@ -21,17 +21,20 @@ class Solution {
         while(t1 != null || t2 != null){
             int sum = carry;
 
-            if(t1 != null)  sum += t1.val;
-            if(t2 != null)  sum += t2.val;
+            if(t1 != null){
+                sum += t1.val;
+                t1 = t1.next;
+            }  
+            if(t2 != null){
+                sum += t2.val;
+                t2 = t2.next;
+            }
 
             ListNode newNode = new ListNode(sum%10);
             carry = sum/10;
 
             temp.next = newNode;
             temp = temp.next;
-
-            if(t1 != null) t1 = t1.next;
-            if(t2 != null) t2 = t2.next;
         }
 
         if(carry != 0){
