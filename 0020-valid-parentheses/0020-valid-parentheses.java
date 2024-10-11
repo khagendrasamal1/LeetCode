369b0b9bb7b9700224b8390cef6819ec40050f43
Khagendra Samal <113529958+khagendrasamal1@users.noreply.github.com>
2024-10-11 19:12:32 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,30 +1,21 @@
 class Solution {
     public boolean isValid(String s) {
-        Stack<Character> gaurav = new Stack<>();
-        int lgth = s.length();
-        for(int i=0;i<lgth;i++){
-            char current = s.charAt(i);
-            if(current == '(' || current == '{'||current == '['){
-                gaurav.push(current);
+        Stack<Character> st = new Stack<Character>();
+
+        for(char it : s.toCharArray()){
+            if(it == '(' || it == '[' || it == '{'){
+                st.push(it);
             }
             else{
-                    if(gaurav.isEmpty())return false; //for edge case "}"
-                    char compliment = gaurav.pop();
-                    if(current == ')'){
-                        if(compliment == '(') continue;
-                        else return false;
-                    }
-                    else if(current == '}'){
-                        if(compliment == '{') continue;
-                        else return false;
-                        }
-                    else if(current == ']'){
-                        if(compliment == '[') continue;
-                        else return false;
-                    }
+                char ch = st.pop();
+                if(st.isEmpty()){
+                    return false;
+                }else{
+                    if((it == ')' && ch=='(') || (it == ']' && ch=='[') || (it == '}' && ch=='{'))  continue;
+                    else return false;
                 }
             }
-        if(!gaurav.isEmpty()) return false; //for edge case "{"
-        return true;
+        }
+        return st.isEmpty();
     }
 }
