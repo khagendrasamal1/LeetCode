56b582e2b28866c14ff44bc9faf6261c97e9f6ec
Khagendra Samal <113529958+khagendrasamal1@users.noreply.github.com>
2025-01-12 10:53:11 +0530

    Time: 16 ms (58.92%), Space: 45.7 MB (50.21%) - LeetHub

--- 2116-check-if-a-parentheses-string-can-be-valid/2116-check-if-a-parentheses-string-can-be-valid.java
+++ 2116-check-if-a-parentheses-string-can-be-valid/2116-check-if-a-parentheses-string-can-be-valid.java
@@ -1,37 +1,34 @@
 class Solution {
     public boolean canBeValid(String s, String locked) {
-        if (s.length() % 2 != 0) return false;
+        int n = s.length();
 
-        // Left to right pass
+        if (n % 2 != 0) return false;
+
         int openCount = 0, wildcards = 0;
-        for (int i = 0; i < s.length(); i++) {
+        for (int i = 0; i < n; i++) {
             char ch = s.charAt(i);
             if (locked.charAt(i) == '1') {
                 if (ch == '(') openCount++;
-                else openCount--; // ')'
+                else openCount--; 
             } else {
                 wildcards++;
             }
-            // Ensure unmatched ')' never exceeds available '(' and wildcards
             if (openCount + wildcards < 0) return false;
         }
 
-        // Right to left pass
         int closeCount = 0;
         wildcards = 0;
-        for (int i = s.length() - 1; i >= 0; i--) {
+        for (int i = n - 1; i >= 0; i--) {
             char ch = s.charAt(i);
             if (locked.charAt(i) == '1') {
                 if (ch == ')') closeCount++;
-                else closeCount--; // '('
+                else closeCount--;
             } else {
                 wildcards++;
             }
-            // Ensure unmatched '(' never exceeds available ')' and wildcards
             if (closeCount + wildcards < 0) return false;
         }
 
-        // If both passes succeed, the string can be valid
         return true;
     }
 }
