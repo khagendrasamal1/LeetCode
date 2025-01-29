971946c24bd6d71f58baeda78017ca0f0394135c
Khagendra Samal <113529958+khagendrasamal1@users.noreply.github.com>
2025-01-29 10:18:30 +0530

    Time: 0 ms (100%), Space: 42.2 MB (59.39%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,6 +1,20 @@
 class Solution {
     public double myPow(double x, int n) {
-        double result = Math.pow(x,n);
-        return result;
+        double ans = 1.0;
+
+        if(n == 0)  return ans;
+
+        long exp = n;
+        boolean isNegative = (n < 0);
+        exp = Math.abs(exp);
+
+        while(exp > 0){
+            if(exp % 2 == 1){
+                ans *= x;
+            }
+            x *= x;
+            exp /= 2;
+        }
+        return isNegative ? 1.0 / ans : ans;
     }
 }
