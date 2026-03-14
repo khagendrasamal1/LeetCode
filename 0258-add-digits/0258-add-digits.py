abaa7708ac6fcff0fc1e0571bb5f6bbd0e0eb95f
Khagendra Samal <113529958+khagendrasamal1@users.noreply.github.com>
2026-03-14 21:12:56 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0258-add-digits/0258-add-digits.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def addDigits(self, num):
+        while num >= 10:
+            sum = 0
+
+            while num > 0:
+                sum += num % 10
+                num //= 10
+
+            num = sum
+        return num
+        
