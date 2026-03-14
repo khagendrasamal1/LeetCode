4e0f5f2972680368df34d62a42e1f33e04c71e77
Khagendra Samal <113529958+khagendrasamal1@users.noreply.github.com>
2026-03-14 20:22:19 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -4,14 +4,5 @@ class Solution(object):
             return 0
         elif n == 1:
             return 1
-        else:
-            a = 0
-            b = 1
-
-            for i in range(n):
-                c = a+b
-                a = b
-                b = c
-
-        return b
+        return self.fib(n-1) + self.fib(n-2)
         
