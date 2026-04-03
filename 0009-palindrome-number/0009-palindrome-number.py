684444f31326574cab9f05d1e390d521c756a4eb
Khagendra Samal <113529958+khagendrasamal1@users.noreply.github.com>
2026-04-03 15:56:37 +0530

    Time: 14 ms (38.7%), Space: 12.3 MB (70.89%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -4,8 +4,8 @@ class Solution(object):
         rev = 0
 
         while n > 0:
-            digit = n % 10
-            rev = rev * 10 + digit
+            digit = n%10
+            rev = rev*10 + digit
             n //= 10
 
         return rev == x
